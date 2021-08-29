5837a1587134efa7171d34705855300269ff85d1
Ivan <oracll90@mail.ru>
2021-08-29 03:12:34 +0300

    final

--- js/script.js
+++ js/script.js
@@ -8,28 +8,29 @@ $(document).ready(function () {
         prevArrow: '<button type="button" class="slick-prev"><img src="../img/left.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../img/right.png"></button>'
     });
-});
 
-const phone = document.querySelector('#phone'),
-    form = document.querySelector('form');
+    const phone = document.querySelector('#phone'),
+        form = document.querySelector('form');
 
-phone.addEventListener('input', (e) => {
-    phone.value = phone.value.replace(/\D/, '');
-});
-
-
-$(function () {
-    $("a[href^='#']").click(function () {
-        var _href = $(this).attr("href");
-        $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
-        return false;
+    phone.addEventListener('input', (e) => {
+        phone.value = phone.value.replace(/\D/, '');
     });
+
+
+    $(function () {
+        $("a[href^='#']").click(function () {
+            var _href = $(this).attr("href");
+            $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+            return false;
+        });
+    });
+
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+
+        form.reset();
+    });
+
+    timer(1800);
 });
 
-form.addEventListener('submit', event => {
-    event.preventDefault();
-
-    form.reset();
-});
-
-timer(1800);
