f7518a7b165557b280262dbcf7336eec5cbf31d7
Ivan <oracll90@mail.ru>
2021-08-29 03:16:38 +0300

    arrows fix

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ $(document).ready(function () {
         autoplay: true,
         slidesToShow: 1,
         speed: 1200,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../img/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/right.png"></button>'
+        prevArrow: '<button type="button" class="slick-prev"><img src="./img/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="./img/right.png"></button>'
     });
 
     const phone = document.querySelector('#phone'),
